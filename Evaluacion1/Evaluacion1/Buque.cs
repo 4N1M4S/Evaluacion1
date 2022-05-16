2cd22397a0761b5b80408f889a2acf7147a30f7b
Ricardo Moller Galdames <moller.ricardo219@gmail.com>
2022-05-16 10:02:55 -0400

    Arreglar el desastre que hice creando la app en otro repositorio

---
+++ Evaluacion1/Evaluacion1/Buque.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluacion1
+{
+    internal class Buque
+    {
+        private int codigo;
+        private string nombre;
+        private string pais;
+        private int cantidadContainers;
+        private int cantidadContainersCargados;
+        private int gastoTransporte;
+        private List<Container> listaContainers;
+
+        /// <summary>
+        /// Constructor solo con código de la clase
+        /// </summary>
+        /// <param name="codigo"></param>
+        public Buque(int codigo)
+        {
+            this.codigo = codigo;
+        }
+
+        /// <summary>
+        /// Constructor completo de la clase
+        /// </summary>
+        /// <param name="codigo"></param>
+        /// <param name="nombre"></param>
+        /// <param name="pais"></param>
+        /// <param name="cantidadContainers"></param>
+        /// <param name="cantidadContainersCargados"></param>
+        /// <param name="gastoTransporte"></param>
+        /// <param name="listaContainers"> Lista de containers almacenados dentro de un buque </param>
+        public Buque(int codigo, string nombre, string pais, int cantidadContainers, int cantidadContainersCargados, int gastoTransporte, List<Container> listaContainers) : this(codigo)
+        {
+            this.nombre = nombre;
+            this.pais = pais;
+            this.cantidadContainers = cantidadContainers;
+            this.cantidadContainersCargados = cantidadContainersCargados;
+            this.gastoTransporte = gastoTransporte;
+            this.listaContainers = listaContainers;
+        }
+    }
+}
---
+++ Evaluacion1/Evaluacion1/Container.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluacion1
+{
+    internal class Container
+    {
+        private int codigo;
+        private string marca;
+        private int cantidadMaxima;
+        private byte tamaño;
+        private bool esRefrigerado;
+        private int pesoActual;
+        public Buque buque;
+
+        /// <summary>
+        /// Constructor solo con código
+        /// </summary>
+        /// <param name="codigo"> Código del container </param>
+        public Container(int codigo)
+        {
+            this.codigo = codigo;
+        }
+
+        /// <summary>
+        /// Constructor con parámetros privados de la clase
+        /// </summary>
+        /// <param name="codigo"></param>
+        /// <param name="marca"></param>
+        /// <param name="cantidadMaxima"></param>
+        /// <param name="tamaño"></param>
+        /// <param name="esRefrigerado"></param>
+        /// <param name="pesoActual"></param>
+        public Container(int codigo, string marca, int cantidadMaxima, byte tamaño, bool esRefrigerado, int pesoActual) : this(codigo)
+        {
+            this.marca = marca;
+            this.cantidadMaxima = cantidadMaxima;
+            this.tamaño = tamaño;
+            this.esRefrigerado = esRefrigerado;
+            this.pesoActual = pesoActual;
+        }
+
+        /// <summary>
+        /// Constructor completo de la clase
+        /// </summary>
+        /// <param name="codigo"></param>
+        /// <param name="marca"></param>
+        /// <param name="cantidadMaxima"></param>
+        /// <param name="tamaño"></param>
+        /// <param name="esRefrigerado"></param>
+        /// <param name="pesoActual"></param>
+        /// <param name="buque"> Código del buque donde está el container </param>
+        public Container(int codigo, string marca, int cantidadMaxima, byte tamaño, bool esRefrigerado, int pesoActual, Buque buque) : this(codigo, marca, cantidadMaxima, tamaño, esRefrigerado, pesoActual)
+        {
+            this.buque = buque;
+        }
+    }
+}
